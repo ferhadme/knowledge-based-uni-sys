8d18c163985b0ffdb22f4bf261c9ac1a55cab2e4
ferhadme <frhdmehdiyev@gmail.com>
2021-10-11 22:08:06 +0400

    modifications, but still buggy

--- asoiu.pl
+++ asoiu.pl
@@ -1,10 +1,8 @@
 %
 
 university(asoiu) .
-
 department(computer_engineering, asoiu) .
 faculty(itif, computer_engineering) .
-
 speciality(it, itif) .
 speciality(cs, itif) .
 speciality(cyber_security, itif) .
@@ -12,22 +10,31 @@ speciality(cyber_security, itif) .
 study(ferhad, it) .
 point(ferhad, 594) .
 no(ferhad, 7) .
+national(ferhad) .
 
 study(ceyhun, it) .
 point(ceyhun, 565) .
 no(ceyhun, 8) .
+national(ceyhun) .
 
 study(ali_abbas, it) .
-
-national(ferhad) .
-national(ceyhun) .
 foreign(ali_abbas) .
+no(ali_abbas, 2) .
+
+study(kamran, cyber_security) .
+point(kamran, 530) .
+no(kamran, 23) .
+national(kamran) .
 
 teacher(ilqar, it) .
 
 
 teach(X, Y) :- 
 	teacher(X, Z), 
+	study(Y, Z) .
+
+groupmate(X, Y) :-
+	study(X, Z),
 	study(Y, Z) .
 
 university_student(X, Y) :- 
@@ -49,13 +56,13 @@ university_member(X, Y) :-
 university_member(X, Y) :- 
 	university_teacher(X, Y) .
 
-scholarship(X, Y) :- 
+scholarship(X) :- 
 	national(X), 
 	point(X, Z), 
 	Z >= 553 .
-private(X, Y) :- 
+private(X) :- 
 	foreign(X) .
-private(X, Y) :- 
+private(X) :- 
 	point(X, Z), Z < 553 .
 
 even_day(X) :- 
@@ -68,25 +75,48 @@ odd_day(X) :-
 	X =< 5, 
 	Z is X mod 2, 
 	Z = 1 .
+top_week(X) :-
+	Z is X mod 2,
+	Z is 1 .
+bottom_week(X) :-
+	Z is X mod 2,
+	Z is 0 .
 
-online(X, Y) :- 
-	odd_day(Y), 
-	no(X, T), 
-	Z is T mod 2, 
-	Z = 1 .
-offline(X, Y) :- 
-	odd_day(Y), 
-	no(X, T), 
-	Z is T mod 2, 
-	Z = 0 .
-online(X, Y) :- 
-	even_day(Y), 
-	no(X, T), 
-	Z is T mod 2, 
-	Z = 0 .
-offline(X, Y) :- 
-	even_day(Y), 
-	no(X, T), 
-	Z is T mod 2, 
-	Z = 1 .
+four_special_student(X, Y) :-
+	Start is (Y - 1) * 4,
+	End is Y * 4,
+	no(X, No),
+	No > Start,
+	No =< End .
+	
+%online(X, Y) :- 
+%	odd_day(Y), 
+%	no(X, T), 
+%	Z is T mod 2, 
+%	Z = 1 .
+%online(X, Y) :- 
+%	even_day(Y), 
+%	no(X, T), 
+%	Z is T mod 2, 
+%	Z = 0 .
+%offline(X, Y) :- 
+%	odd_day(Y), 
+%	no(X, T), 
+%	Z is T mod 2, 
+%	Z = 0 .
+%offline(X, Y) :- 
+%	even_day(Y), 
+%	no(X, T), 
+%	Z is T mod 2, 
+%	Z = 1 .
+
+% stud, weekno, day
+offline(X, Y, Z) :-
+	four_special_student(X, Y) .
+	
+offline(X, Y, Z) :-
+	
+
+online(X, Y, Z) :-
+	\+ offline(X, Y, Z) .
 
