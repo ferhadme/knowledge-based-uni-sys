690386bc9cec5111e7ee72bc999e9513d9b1a923
ferhadme <frhdmehdiyev@gmail.com>
2021-10-11 18:21:55 +0400

    formatting

--- asoiu.pl
+++ asoiu.pl
@@ -25,25 +25,68 @@ foreign(ali_abbas) .
 
 teacher(ilqar, it) .
 
-teach(X, Y) :- teacher(X, Z), study(Y, Z) .
 
-university_student(X, Y) :- study(X, Z), speciality(Z, U), faculty(U, T), department(T, Y), university(Y) .
-university_teacher(X, Y) :- teacher(X, Z), speciality(Z, U), faculty(U, T), department(T, Y), university(Y) .
+teach(X, Y) :- 
+	teacher(X, Z), 
+	study(Y, Z) .
 
-university_member(X, Y) :- university_student(X, Y) .
-university_member(X, Y) :- university_teacher(X, Y) .
+university_student(X, Y) :- 
+	study(X, Z), 
+	speciality(Z, U), 
+	faculty(U, T), 
+	department(T, Y), 
+	university(Y) .
 
-scholarship(X, Y) :- national(X), point(X, Z), Z >= 553 .
-private(X, Y) :- foreign(X) .
-private(X, Y) :- point(X, Z), Z < 553 .
+university_teacher(X, Y) :- 
+	teacher(X, Z), 
+	speciality(Z, U), 
+	faculty(U, T), 
+	department(T, Y), 
+	university(Y) .
 
+university_member(X, Y) :- 
+	university_student(X, Y) .
+university_member(X, Y) :- 
+	university_teacher(X, Y) .
 
-% online-offline
-even_day(X) :- X >= 1, X =< 5, Z is X mod 2, Z = 0 .
-odd_day(X) :- X >= 1, X =< 5, Z is X mod 2, Z = 1 .
+scholarship(X, Y) :- 
+	national(X), 
+	point(X, Z), 
+	Z >= 553 .
+private(X, Y) :- 
+	foreign(X) .
+private(X, Y) :- 
+	point(X, Z), Z < 553 .
 
-online(X, Y) :- odd_day(Y), no(X, T), Z is T mod 2, Z = 1 .
-offline(X, Y) :- odd_day(Y), no(X, T), Z is T mod 2, Z = 0 .
-online(X, Y) :- even_day(Y), no(X, T), Z is T mod 2, Z = 0 .
-offline(X, Y) :- even_day(Y), no(X, T), Z is T mod 2, Z = 1 .
+even_day(X) :- 
+	X >= 1, 
+	X =< 5, 
+	Z is X mod 2, 
+	Z = 0 .
+odd_day(X) :- 
+	X >= 1, 
+	X =< 5, 
+	Z is X mod 2, 
+	Z = 1 .
+
+online(X, Y) :- 
+	odd_day(Y), 
+	no(X, T), 
+	Z is T mod 2, 
+	Z = 1 .
+offline(X, Y) :- 
+	odd_day(Y), 
+	no(X, T), 
+	Z is T mod 2, 
+	Z = 0 .
+online(X, Y) :- 
+	even_day(Y), 
+	no(X, T), 
+	Z is T mod 2, 
+	Z = 0 .
+offline(X, Y) :- 
+	even_day(Y), 
+	no(X, T), 
+	Z is T mod 2, 
+	Z = 1 .
 
